90c0b5e0c011c2528277b2a06a07317c0493a393
Matthew Giamou <matthew.giamou@gmail.com>
2020-12-09 16:21:33 -0500

    Add mostly working example (need to fix one clique and make it into a unit test).

--- graphik/solvers/sdp_snl.py
+++ graphik/solvers/sdp_snl.py
@@ -2,16 +2,17 @@
 Rank-{2,3} SDP relaxation tailored to sensor network localization (SNL) applied to our DG/QCQP IK formulation.
 """
 import numpy as np
-import scipy as sc
 import networkx as nx
 
-from graphik.graphs.graph_base import RobotRevoluteGraph
 from graphik.utils.roboturdf import load_ur10
-from graphik.utils.utils import flatten
 from graphik.robots.robot_base import RobotRevolute
+from graphik.solvers.constraints import get_full_revolute_nearest_point
 
 
 def linear_matrix_equality(i: int, j: int, n_vars: int) -> np.ndarray:
+    """
+    Convert a distance constraint into a LME for internal variables (no constant so no linear term).
+    """
     A = np.zeros((n_vars, n_vars))
     A[i, i] = 1.
     A[j, j] = 1.
@@ -21,6 +22,9 @@ def linear_matrix_equality(i: int, j: int, n_vars: int) -> np.ndarray:
 
 
 def linear_matrix_equality_with_anchor(i: int, n_vars: int, ee: np.ndarray) -> np.ndarray:
+    """
+    Convert a distance constraint into a LME for an internal variable and a constant end-effector (needs homog. vars).
+    """
     A = np.zeros((n_vars, n_vars))
     d = len(ee)
     A[i, i] = 1.
@@ -32,6 +36,8 @@ def linear_matrix_equality_with_anchor(i: int, n_vars: int, ee: np.ndarray) -> n
 def distance_clique_linear_map(graph: nx.Graph, clique: frozenset,
                                ee_assignments: dict = None, ee_cost: bool = False) -> (list, list, dict):
     """
+    Produce a set of SDP-relaxed constraints in the form of A: S^n -> R^m (with constant values b) for a clique of
+    variables in a DG problem.
 
     """
     ees_clique = [key for key in ee_assignments if key in clique]
@@ -60,17 +66,17 @@ def distance_clique_linear_map(graph: nx.Graph, clique: frozenset,
                         continue
                     elif u in ees_clique:
                         A_uv = linear_matrix_equality_with_anchor(A_mapping[v], n, ee_assignments[u])
-                        b_uv = graph[u][v]['weight'] - ee_assignments[u].dot(ee_assignments[u])
+                        b_uv = graph[u][v]['weight']**2 - ee_assignments[u].dot(ee_assignments[u])
                     elif v in ees_clique:
                         A_uv = linear_matrix_equality_with_anchor(A_mapping[u], n, ee_assignments[v])
-                        b_uv = graph[u][v]['weight'] - ee_assignments[v].dot(ee_assignments[v])
+                        b_uv = graph[u][v]['weight']**2 - ee_assignments[v].dot(ee_assignments[v])
                     else:
                         A_uv = linear_matrix_equality(A_mapping[u], A_mapping[v], n)
-                        b_uv = graph[u][v]['weight']  # TODO: squared or not? I forget
+                        b_uv = graph[u][v]['weight']**2
 
                 else:
                     A_uv = linear_matrix_equality(A_mapping[u], A_mapping[v], n)
-                    b_uv = graph[u][v]['weight']  # TODO: squared or not? I forget
+                    b_uv = graph[u][v]['weight']**2  # TODO: squared or not? I forget
                 A.append(A_uv)
                 b.append(b_uv)
                 A_mapping[frozenset((u, v))] = constraint_idx
@@ -81,8 +87,7 @@ def distance_clique_linear_map(graph: nx.Graph, clique: frozenset,
 def distance_constraints(robot: RobotRevolute, end_effectors: dict, sparse: bool=False,
                          ee_cost: bool=False) -> (np.ndarray, dict):
     """
-    Produce a linear mapping S -> R for the equality constraints describing
-    our DG problem instance.
+    Produce an SDP-relaxed linear mapping for the equality constraints describing our DG problem instance.
 
     :param robot:
     :param end_effectors: dict of end-effector assignments
@@ -108,12 +113,39 @@ def distance_constraints(robot: RobotRevolute, end_effectors: dict, sparse: bool
     return clique_dict
 
 
+def evaluate_linear_map(clique: frozenset, A:list, b: list, mapping: dict, input_vals: dict) -> list:
+    """
+    Evaluate the linear map given by A, b, mapping over the variables in clique for input_vals.
+    """
+    n = len(clique)
+    d = len(list(input_vals.values())[0])
+    X = np.zeros((d, 0))
+    for var in clique:
+        if var in mapping:
+            # X[:, mapping[var]] = input_vals[var]
+            X = np.hstack([X, input_vals[var][None].T])
+    if A[0].shape[0] > n:
+        # assert d == A[0].shape[0] - n, print(f"len(A): {A[0].shape[0]}, n:{n}")
+        X = np.hstack([X, np.eye(d)])
+    Z = X.T@X
+    output = [np.trace(A[idx]@Z) - b[idx] for idx in range(len(A))]
+
+    return output
+
+
 if __name__ == '__main__':
-    sparse = False
+    sparse = True
     ee_cost = False
-    end_effectors = {'p0': np.array([0., 0., 0.]),
-                     'q0': np.array([0., 0., 1.]),
-                     'p6': np.array([1., 1., 1.]),
-                     'q6': np.array([1., 1., 2.])}
     robot, graph = load_ur10()
+    q = robot.random_configuration()
+    full_points = [f'p{idx}' for idx in range(0, graph.robot.n + 1)] + \
+                  [f'q{idx}' for idx in range(0, graph.robot.n + 1)]
+    input_vals = get_full_revolute_nearest_point(graph, q, full_points)
+    end_effectors = {key: input_vals[key] for key in ['p0', 'q0', 'p6', 'q6']}
+
     constraint_clique_dict = distance_constraints(robot, end_effectors, sparse, ee_cost)
+    A, b, mapping = list(constraint_clique_dict.values())[0]
+
+    for clique in constraint_clique_dict:
+        A, b, mapping = constraint_clique_dict[clique]
+        print(evaluate_linear_map(clique, A, b, mapping, input_vals))
