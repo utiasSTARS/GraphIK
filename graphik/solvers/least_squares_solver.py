cc412b774cccf92db079136339ae6331f6e54068
Filip Maric <filip.maric@robotics.utias.utoronto.ca>
2024-05-12 18:21:06 -0400

    add limits to least squares solver

--- graphik/solvers/least_squares_solver.py
+++ graphik/solvers/least_squares_solver.py
@@ -34,6 +34,23 @@ def jacobian_(x, A):
     return A.dot(x).reshape(-1, x.size)
 
 
+def residual_limits_(x, A, U, L, d, u, l):
+    e0 = A.dot(x).reshape(-1, x.size).dot(x) - d
+    e1 = np.maximum(U.dot(x).reshape(-1, x.size).dot(x) - u, 0)
+    e2 = np.minimum(L.dot(x).reshape(-1, x.size).dot(x) - l, 0)
+    return np.hstack((e0,e1,e2))
+
+
+def jacobian_limits_(x, A, U, L, u, l):
+    j0 = A.dot(x).reshape(-1, x.size)
+    j1 = U.dot(x).reshape(-1, x.size)
+    j2 = L.dot(x).reshape(-1, x.size)
+    m1 = j1.dot(x) - u < 0
+    m2 = j2.dot(x) - l > 0
+    j1[m1] = 0
+    j2[m2] = 0
+    return np.vstack((2*j0, 2*j1, 2*j2))
+
 def residual_loop_(Y, D_goal, inds, dim):
     num_dist = inds[0].size
     res = np.zeros(num_dist)
@@ -65,31 +82,35 @@ def jacobian_loop_(Y, inds, dim):
 np.set_printoptions(threshold=sys.maxsize)
 
 class LeastSquaresSolver:
-    def __init__(self, graph: ProblemGraph, cost_type='dense', jit=False, *args, **kwargs):
+    def __init__(self, graph: ProblemGraph, cost_type='sparse', jit=False, *args, **kwargs):
         for key in kwargs:
             setattr(self, key, kwargs[key])
         self.graph = graph
         self.dim = graph.dim
         self.N = graph.number_of_nodes()
         self.cost_type = cost_type
-
-        if not hasattr(self, 'cost_type'):
-            self.cost_type = 'loop'
-        if not hasattr(self, 'jit'):
-            self.cost_type = 'loop'
+        self.jit = jit
 
         if self.cost_type == "loop":
             self.residual_ = residual_loop_
             self.jacobian_ = jacobian_loop_
+            self.residual_limits_ = None
+            self.jacobian_limits_ = None
         elif self.cost_type == "sparse" or "dense":
             self.residual_ = residual_
             self.jacobian_ = jacobian_
+            self.residual_limits_ = residual_limits_
+            self.jacobian_limits_ = jacobian_limits_
         else:
             raise NotImplementedError(f"Cost {self.cost_type} not implemented.")
 
         if jit and jitted_f:
             self.residual_ = jitted_f(self.residual_)
             self.jacobian_ = jitted_f(self.jacobian_)
+            if self.residual_limits_:
+                self.residual_limits_ = jitted_f(self.residual_limits_)
+            if self.jacobian_limits_:
+                self.jacobian_limits_ = jitted_f(self.jacobian_limits_)
 
     def generate_initialization(self, bounds, dim, omega, psi_L, psi_U):
         # Generates a random EDM within the set bounds
@@ -111,7 +132,6 @@ class LeastSquaresSolver:
         if self.cost_type == "loop":
             residual = lambda x: self.residual_(x, D_goal, inds, dim)
             jacobian = lambda x: self.jacobian_(x, inds, dim)
-
         else:
             sparse = self.cost_type == "sparse"
             res_sq_vec_batch = sum_square_op_batched(
@@ -120,6 +140,32 @@ class LeastSquaresSolver:
             residual = lambda Y: residual_(Y, res_sq_vec_batch, d)
             diff_sq_vec_batch = 2*res_sq_vec_batch
             jacobian = lambda Y: jacobian_(Y, diff_sq_vec_batch)
+
+        return residual, jacobian
+
+    def create_cost_limits(self, D_goal, omega, psi_L, psi_U):
+        diff = psi_L != psi_U
+        LL = diff * (psi_L > 0) * (~omega.astype(bool))
+        UU = diff * (psi_U > 0) * (~omega.astype(bool))
+
+        L = sum_square_op_batched(
+            LL, self.dim, vectorized=True, sparse=not self.jit, flat=True
+        )
+        U = sum_square_op_batched(
+            UU, self.dim, vectorized=True, sparse=not self.jit, flat=True
+        )
+        A = sum_square_op_batched(
+            omega, self.dim, vectorized=True, sparse=not self.jit, flat=True
+        )
+
+        dist = D_goal[np.nonzero(np.triu(omega))]
+        dist_L = psi_L[np.nonzero(np.triu(LL))]
+        dist_U = psi_U[np.nonzero(np.triu(UU))]
+
+        residual = lambda Y: self.residual_limits_(
+            Y, A, U, L, dist, dist_U, dist_L
+        )
+        jacobian = lambda Y: self.jacobian_limits_(Y, A, U, L, dist_U, dist_L)
 
         return residual, jacobian
 
@@ -138,12 +184,15 @@ class LeastSquaresSolver:
         Implementation of solver using base Scipy unconstrained optimization algorithms.
         Currently does not support limits.
         """
+        psi_L, psi_U = self.graph.distance_bound_matrices()
         # Generate cost, gradient and hessian-vector product
-        residual, jacobian = self.create_cost(D_goal, omega)
+        if use_limits:
+            residual, jacobian = self.create_cost_limits(D_goal, omega, psi_L, psi_U)
+        else:
+            residual, jacobian = self.create_cost(D_goal, omega)
 
         # Generate initialization
         # if Y_init is None:
-        psi_L, psi_U = self.graph.distance_bound_matrices()
         Y_init = self.generate_initialization(bounds, self.dim, omega, psi_L, psi_U)
         Y_init = np.ascontiguousarray(Y_init.flatten())
         start_time = time.time()
@@ -168,107 +217,3 @@ class LeastSquaresSolver:
             return optlog
         else:
             return sol.x
-
-
-if __name__ == "__main__":
-    from scipy.sparse import csc_array
-    from scipy.sparse.csgraph import reverse_cuthill_mckee
-    from scipy.linalg import norm
-    from graphik.utils.roboturdf import load_kuka, load_ur10
-    from experiments.problem_generation import generate_revolute_problem
-
-    robot, graph = load_ur10()
-    G, T_goal, D_goal, X_goal = generate_revolute_problem(graph)
-    omega = adjacency_matrix_from_graph(G)
-    inds = np.nonzero(np.triu(omega))
-    N = omega.shape[0]
-    dim = robot.dim
-    num_dist = len(inds[0])
-    M = num_dist * dim
-    idx, jdx = inds
-
-    # Incidence matrix, implements p_i - p_j for p -> (N,dim)
-    B = incidence_matrix_from_adjacency(omega)
-
-    # Implements p_i - p_j for p -> N*dim
-    B_vec = np.kron(B, np.eye(dim))
-
-    # Implements sum((p_i - p_j)**2) as p.T @ A @ p
-    res_sum_sq = B.T.dot(B)
-    res_sum_sq_vec = B_vec.T @ B_vec
-
-    # Implements d[i] = (p[i] - p[j])**2 = p @ A[i] @ p
-    res_sq_vec_all = [
-        np.kron(np.outer(B[i], B[i]), np.eye(dim)) for i in range(num_dist)
-    ]
-
-    # Implements batched d = (p[all_i] - p[all_j])**2
-    res_sq_vec_batch = np.stack(res_sq_vec_all)
-    res_sq_vec_batch_inds = np.nonzero(res_sq_vec_batch)
-    res_sq_vec_batch_data = res_sq_vec_batch[res_sq_vec_batch_inds]
-    res_sq_vec_batch_sparse = csc_array(
-        np.ascontiguousarray(res_sq_vec_batch.swapaxes(0, 1).reshape(N * dim, -1).T)
-    )
-
-    # Implements the gradient for batched d w.r.t p
-    diff_sq_vec_batch = -2 * res_sq_vec_batch
-    diff_sq_vec_batch_inds = np.nonzero(diff_sq_vec_batch)
-    diff_sq_vec_batch_data = diff_sq_vec_batch[diff_sq_vec_batch_inds]
-    diff_sq_vec_batch_sparse = csc_array(
-        np.ascontiguousarray(diff_sq_vec_batch.swapaxes(0, 1).reshape(N * dim, -1).T)
-    )
-
-    d = D_goal[inds]
-    sel = np.arange(0, num_dist * dim, dim)
-    Y = np.ones(N * 3)
-
-    num_runs = 10000  # Number of times to run the timing test
-    setup_code = """
-from __main__ import f, Y
-"""
-    print("RESIDUAL TIMING")
-
-    def f(Y):
-        Y = Y.reshape(N, 3)
-        return d - np.diag(np.linalg.multi_dot((B, Y, Y.T, B.T)))
-
-    time_taken = timeit.timeit("f(Y)", setup=setup_code, number=num_runs)
-    print(f"Average time per run: {time_taken / num_runs:.6f} seconds")
-
-    def f(Y):
-        return d - np.sum(B_vec.dot(Y).reshape(num_dist, dim) ** 2, axis=1)
-
-    time_taken = timeit.timeit("f(Y)", setup=setup_code, number=num_runs)
-    print(f"Average time per run: {time_taken / num_runs:.6f} seconds")
-
-    def f(Y):
-        res = np.diag(np.linalg.multi_dot((B_vec, Y[:, None], Y[None, :], B_vec.T)))
-        return d - np.add.reduceat(res, sel)
-
-    time_taken = timeit.timeit("f(Y)", setup=setup_code, number=num_runs)
-    print(f"Average time per run: {time_taken / num_runs:.6f} seconds")
-
-    def f(Y):
-        return d - Y.dot(res_sq_vec_batch.dot(Y[:, None]))
-
-    time_taken = timeit.timeit("f(Y)", setup=setup_code, number=num_runs)
-    print(f"Average time per run: {time_taken / num_runs:.6f} seconds")
-
-    print("JACOBIAN TIMING")
-
-    diff_sq_vec_batch_flat = np.ascontiguousarray(
-        diff_sq_vec_batch.swapaxes(0, 1).reshape(N * dim, -1).T
-    )
-
-    def f(Y):
-        return diff_sq_vec_batch_flat.dot(Y).reshape(num_dist, dim * N)
-
-    time_taken = timeit.timeit("f(Y)", setup=setup_code, number=num_runs)
-    print(f"Average time per run: {time_taken / num_runs:.6f} seconds")
-
-    def f(Y):
-        return diff_sq_vec_batch_sparse.dot(Y).reshape(num_dist, dim * N)
-
-    time_taken = timeit.timeit("f(Y)", setup=setup_code, number=num_runs)
-
-    print(f"Average time per run: {time_taken / num_runs:.6f} seconds")
